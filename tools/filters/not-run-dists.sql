3008539c3814acfd74e6dc76d1d3b38a77a988b1
djerius@cfa.harvard.edu <djerius@cfa.harvard.edu>
2014-03-27 17:26:23 -0400

    if dists are partially run, need to take that into account when seeing which haven't been run

--- tools/filters/not-run-dists.sql
+++ tools/filters/not-run-dists.sql
@@ -2,6 +2,8 @@ select distinct author || '/' || archive as authdist
     from xs, archive
     where xs.distribution = archive.distribution
       and xs.version_numified = archive.version
-      and authdist not in ( select distribution from pass )
+      and 
+        ( select count(*) from xs where distribution = archive.distribution )
+	!= ( select count(*) distribution from pass where distribution = authdist )
 ;
 
